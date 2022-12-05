bb69325bcbe8f6a1b915ecffa39479a6d86a58fd
Mohitk391 <mohitkpatel391@gmail.com>
2022-12-05 18:30:41 +0530

    feat: intial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Homepage } from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} /> */}
+      </Routes>
     </div>
   );
 }
