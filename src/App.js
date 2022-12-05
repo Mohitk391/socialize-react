c354e85727bf29d24f946395e5158d21549b94e0
Mohitk391 <mohitkpatel391@gmail.com>
2022-12-05 20:16:14 +0530

    feat: initial setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Bookmarks } from './pages/bookmarks/Bookmarks';
 import { Homepage } from './pages/homepage/Homepage';
+import { Profile } from './pages/profile/Profile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/bookmarks" element={<Bookmarks />} />
+        <Route path="/profile" element={<Profile />} />
         {/* <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} /> */}
       </Routes>
