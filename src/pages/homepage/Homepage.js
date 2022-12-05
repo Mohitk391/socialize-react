9e8e9f0e2e8bf8db51647ea9359c7fb5bba78a4b
Mohitk391 <mohitkpatel391@gmail.com>
2022-12-05 18:59:55 +0530

    refactor: sidebar components added

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,19 +1,14 @@
 import { NavBar } from "../../components/NavBar/NavBar";
-import "./Homepage.css";
+import "../../App.css";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
+import { AccountSidebar } from "../../components/AccountSidebar/AccountSidebar";
 
 function Homepage() {
     return (
         <div className="page-layout">
             <NavBar />
             <main className="feed-body d-flex justify-content-evenly gap-4 p-3">
-                <section className="sidebar d-flex flex-column align-self-start gap-3 px-2 flex-grow-0" >
-                    <div className="navigation-menu d-flex flex-column gap-3">
-                        <div className="d-flex gap-3 fs-5 fw-bold"><i className="bi bi-house-door-fill"></i> <a className="text-decoration-none text-black" href="/index.html">Home</a></div>
-                        <div className="d-flex gap-3 fs-5"><i className="bi bi-bookmark"></i><a className="text-decoration-none text-black" href="/bookmarks.html">Bookmarks</a></div>
-                        <div className="d-flex gap-3 fs-5"><i className="bi bi-person-circle"></i><a className="text-decoration-none text-black" href="/profile.html">Profile</a></div>
-                    </div>
-                    <button className="btn btn-outline-primary">Create New Post</button>
-                </section>
+                <Sidebar />
                 <div className="content-section d-flex flex-column gap-3 w-50 flex-grow-1">
                     <div className="feed-input bg-white p-3 d-flex gap-2 rounded">
                             <div className="feed-avatar flex-shrink-1" >
@@ -130,74 +125,7 @@ function Homepage() {
                         </div>
                     </div>
                 </div>
-                <div className="accounts-suggestion d-flex flex-column align-self-start flex-grow-0 gap-2 bg-white px-3 py-2 rounded" >
-                    <div className="accounts-title d-flex justify-content-between align-items-center gap-5 text-nowrap">
-                        <span className="fw-bolder">Suggested Accounts</span>
-                        <button className="btn text-primary px-0">Show More</button>
-                    </div>
-                    <div className="suggested-accounts d-flex flex-column gap-2">
-                        <div className="user-account d-flex">
-                            <div className="feed-avatar" >
-                                <a className="avatar-image d-flex justify-content-center" href="/profile.html">
-                                    <img src="/img_avatar.png" className=" img-fluid rounded-circle w-50" alt="avatar" />
-                                </a>
-                            </div>
-                            <div className="user details d-flex justify-content-between w-100 align-items-start">
-                                <div className="username">
-                                    <div className="fw-bolder">User Name</div>
-                                </div>
-                                <div className="connect-action">
-                                    <button className="btn text-primary p-0">Connect+</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="user-account d-flex">
-                            <div className="feed-avatar" >
-                                <a className="avatar-image d-flex justify-content-center" href="/profile.html">
-                                    <img src="/img_avatar.png" className=" img-fluid rounded-circle w-50" alt="avatar" />
-                                </a>
-                            </div>
-                            <div className="user details d-flex justify-content-between w-100 align-items-start">
-                                <div className="username">
-                                    <span className="fw-bolder">User Name</span>
-                                </div>
-                                <div className="connect-action">
-                                    <button className="btn text-primary p-0">Connect+</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="user-account d-flex">
-                            <div className="feed-avatar" >
-                                <a className="avatar-image d-flex justify-content-center" href="/profile.html">
-                                    <img src="/img_avatar.png" className=" img-fluid rounded-circle w-50" alt="avatar" />
-                                </a>
-                            </div>
-                            <div className="user details d-flex justify-content-between w-100 align-items-start">
-                                <div className="username">
-                                    <span className="fw-bolder">User Name</span>
-                                </div>
-                                <div className="connect-action">
-                                    <button className="btn text-primary p-0">Connect+</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="user-account d-flex">
-                            <div className="feed-avatar" >
-                                <a className="avatar-image d-flex justify-content-center" href="/profile.html">
-                                    <img src="/img_avatar.png" className=" img-fluid rounded-circle w-50" alt="avatar" />
-                                </a>
-                            </div>
-                            <div className="user details d-flex justify-content-between w-100 align-items-start">
-                                <div className="username">
-                                    <span className="fw-bolder">User Name</span>
-                                </div>
-                                <div className="connect-action">
-                                    <button className="btn text-primary p-0">Connect+</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <AccountSidebar />
             </main>
         </div>
     )
