021f006b0a4a5f80b946d59682b7a16b05c5cd19
Mohitk391 <mohitkpatel391@gmail.com>
2022-12-07 16:12:58 +0530

    refactor: routes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Login } from './pages/authentication/Login';
+import { Signup } from './pages/authentication/Signup';
 import { Bookmarks } from './pages/bookmarks/Bookmarks';
 import { Homepage } from './pages/homepage/Homepage';
 import { Profile } from './pages/profile/Profile';
@@ -11,8 +13,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/bookmarks" element={<Bookmarks />} />
         <Route path="/profile" element={<Profile />} />
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,24 @@
+import { Link } from "react-router-dom";
 
 function NavBar(){
 
     return (
         <nav className="navbar px-3 align-items-center">
                 <div className="site-title fs-3 fw-bold fst-italic">
-                    <a className="text-decoration-none text-primary" href="/index.html">SOCIALIZE</a>
+                    <Link className="text-decoration-none text-primary" to="/">SOCIALIZE</Link>
                 </div>
                 <div>
                     <div className="navbar-search input-group">
                         <input className="navbar-searchbar form-control" type="text" placeholder="Search Posts, People, Anything..." size="40"/>
                         <button className="btn btn-outline-primary" type="button" id="button-addon2">
-                            <a href="/">
+                            <Link to="/">
                                 <i className="bi bi-search text-black"></i>
-                            </a>
+                            </Link>
                         </button>
                     </div>
                 </div>
                 <div className="nav-bar-buttons">
-                    <a href="/login.html"><button className="btn btn-hover">Login</button></a>
+                    <Link to="/login"><button className="btn btn-hover">Login</button></Link>
                 </div>
         </nav>
     );
