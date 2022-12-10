2420f5a7598010f366e967dcd7bf8d7fb9f54d94
Mohitk391 <mohitkpatel391@gmail.com>
2022-12-10 19:54:28 +0530

    feat: user functionality added

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,21 +1,52 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./authentication.css";
+import {useUser} from "../../contexts/UserContext";
 
 function Login(){
+    const [user, setUser] = useState({ mobile: "", password: "", mobileError: false, passwordError: false});
+    const { userDispatch } = useUser();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const login = e => {
+        e.preventDefault();
+        if((user.mobile.trim()).length === 0 || (user.password.trim()).length === 0){
+            if((user.mobile.trim()).length===0)
+                setUser({...user, mobileError: true});
+            if((user.password.trim()).length === 0)
+                setUser({...user, passwordError: true});
+        }
+        else{
+            if(user.mobile=== "9876543210" && user.password==="mohit123"){
+                userDispatch({type: "SET_USER", value: "Mohit_Patel123"});
+                localStorage.setItem("token", "Mohit_Patel123");
+                const from = location.state?.from || '/';
+                navigate(from, { replace: true });
+            }
+            else{
+                if(user.mobile!=="9876543210")
+                    setUser({...user, mobileError: true});
+                else
+                    setUser({...user, passwordError: true});
+            }
+        }
+    }
+
     return (
         <div className="authentication-page-layout">
             <div className="d-flex justify-content-center align-items-center  flex-column h-50 w-auto gap-3">
             <div className="site-title fs-1 fw-bold fst-italic"><Link className="text-decoration-none text-white" to="/">SOCIALIZE</Link></div>
-            <div className="login-body d-flex flex-column align-items-center bg-white px-5 shadow rounded-4">
+            <form className="login-body d-flex flex-column align-items-center bg-white px-5 shadow rounded-4" onSubmit={(e)=>login(e)}>
                 <div className="login-title fs-4 fw-bold pt-2 px-2">Login</div>
-                <div className="body d-flex flex-column align-items-center gap-2 p-4">
-                    <div className="input-field d-flex flex-column ">
-                        <label className="fw-bold" for="mobile">Mobile</label>
-                        <input className="form-control"  type="number" id="mobile" placeholder="Enter Mobile No." />
+                <div className="body d-flex flex-column align-items-center gap-3 p-4">
+                    <div className="input-field form-floating d-flex flex-column ">
+                        <input className={`${user.mobileError ? "border-danger" : ""} form-control`} type="number" id="mobile" placeholder="Enter Mobile No." onChange={(e)=>setUser({...user, mobile: e.target.value, mobileError: false})}/>
+                        <label  htmlFor="mobile">Mobile</label>
                     </div>
-                    <div className="input-field d-flex flex-column ">
-                        <label className="fw-bold" for="password">Password</label>
-                        <input  className="form-control" type="password" id="password" placeholder="Enter Password" />
+                    <div className="input-field form-floating d-flex flex-column ">
+                        <input  className={`${user.passwordError ? "border-danger" : ""} form-control`} type="password" id="password" placeholder="Enter Password" onChange={(e)=>setUser({...user, password: e.target.value, passwordError: false})}/>
+                        <label htmlFor="password">Password</label>
                     </div>
                     <div className="forgot-pwd">
                         <small><a className="text-decoration-none text-start text-danger" href="/">Forgot Password?</a></small>
@@ -25,7 +56,7 @@ function Login(){
                         <p><Link className="text-decoration-none text-reset" to="/signup">Create new Account</Link></p>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
     );
--- src/pages/authentication/Signup.js
+++ src/pages/authentication/Signup.js
@@ -1,33 +1,67 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useUser } from "../../contexts/UserContext";
 
 function Signup(){
+    const navigate = useNavigate();
+    const { userDispatch } = useUser();
+    const [user, setUser] = useState({name:"", mobile: "", password: "", confirmPassword: "", nameError: false, mobileError: false, passwordError: false, confirmPasswordError: false});
+
+    const signup = async (e) => {
+        e.preventDefault();
+        if((user.name.trim()).length === 0 || (user.mobile.trim()).length===0 || (user.email.trim()).length ===0 || (user.password.trim()).length ===0){
+            if((user.name.trim()).length === 0)
+                setUser({...user, nameError: true});
+            if((user.mobile.trim()).length === 0)
+                setUser({...user, mobileError: true});
+            if((user.password.trim()).length === 0)
+                setUser({...user, passwordError: true});
+        }
+        else{
+            userDispatch({type: "SET_USER", value: "Mohit_Patel123"});
+            localStorage.setItem("token", "Mohit_Patel123");
+            navigate("/");
+        }
+        
+    }
+
+    const checkConfirmPassword = (e) => {
+        setUser({...user, confirmPassword: e.target.value});
+        if(user.password !== e.target.value){
+            setUser({...user, confirmPasswordError: true});
+        }
+        else {
+            setUser({...user, confirmPasswordError: false});
+        }
+    }
+
     return (
         <div className="authentication-page-layout">
             <div className="d-flex justify-content-center align-items-center  flex-column h-50 w-auto gap-3">
                 <div className="site-title fs-1 fw-bold fst-italic"><Link className="text-decoration-none text-white" to="/">SOCIALIZE</Link></div>
                 <div className="login-body d-flex flex-column align-items-center bg-white px-5 shadow rounded-4">
                     <div className="login-title fs-4 fw-bold pt-2 px-2">Signup</div>
-                    <div className="body d-flex flex-column align-items-center gap-3 px-4 pt-4 ">
+                    <form className="body d-flex flex-column align-items-center gap-3 px-4 pt-4 " onSubmit={(e)=>signup(e)}>
                         <div className="row">
                             <div className="col input-field form-floating d-flex flex-column">
-                                <input className="form-control"  type="text" id="name" placeholder="Full Name"/>
+                                <input className={`${user.nameError ? "border-danger" : ""} form-control`}  type="text" id="name" placeholder="Full Name" onChange={(e)=>setUser({...user, name: e.target.value, nameError: false})}/>
                                 <label for="name">Full Name</label>
                             </div>
                             <div className="col input-field form-floating d-flex flex-column">
-                                <input className="form-control"  type="number" id="mobile" placeholder="Mobile Number" />
+                                <input className={`${user.mobileError ? "border-danger" : ""} form-control`}  type="number" id="mobile" placeholder="Mobile Number" onChange={(e)=>setUser({...user, mobile: e.target.value, mobileError: false})}/>
                                 <label for="mobile">Mobile</label>
                             </div>
                         </div>
                         <div className="input-field form-floating d-flex flex-column w-100">
-                            <input className="form-control"  type="email" id="email" placeholder="Email Address" />
+                            <input className={`${user.emailError ? "border-danger" : ""} form-control`}  type="email" id="email" placeholder="Email Address" onChange={(e)=>setUser({...user, email: e.target.value, emailError: false})}/>
                             <label htmlFor="email">Email Address    </label>
                         </div>
                         <div className="input-field form-floating d-flex flex-column w-100">
-                            <input className="form-control"  type="password" id="password" placeholder="Password" />
+                            <input className={`${user.passwordError ? "border-danger" : ""} form-control`}  type="password" id="password" placeholder="Password"onChange={(e)=>setUser({...user, password: e.target.value, passwordError: false})}/>
                             <label htmlFor="password">Password</label>
                         </div>
                         <div className="input-field form-floating d-flex flex-column w-100">
-                            <input className="form-control"  type="password" id="confirmPassword" placeholder="Confirm Password" />
+                            <input className={`${user.confirmPasswordError ? "border-danger" : ""} form-control`}  type="password" id="confirmPassword" placeholder="Confirm Password" onChange={(e)=>checkConfirmPassword(e)}/>
                             <label htmlFor="confirmPassword">Confirm Password</label>
                         </div>
                         <div className="signup-btn w-100">
@@ -36,7 +70,7 @@ function Signup(){
                         <div className="login-option">
                             <p><Link className="text-decoration-none text-reset" to="/login">Already have an Account? </Link></p>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
